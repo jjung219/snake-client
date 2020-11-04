8d0b2f45c6f989b3bdf5861fd2b41bac2469c40d
Jennifer Jung <jenjung2194@gmail.com>
2020-11-04 20:25:22 +0000

    WASD movement completed

--- input.js
+++ input.js
@@ -1,15 +1,9 @@
+let connection;
 const stdin = process.stdin;
 
-const handleUserInput = () => {
-  stdin.on('data', key => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-}
-
-const setupInput = () => {
+const setupInput = conn => {
   const stdin = process.stdin;
+  connection = conn;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
@@ -17,6 +11,26 @@ const setupInput = () => {
   handleUserInput();
 
   return stdin;
+}
+
+const handleUserInput = () => {
+  stdin.on('data', key => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    if (key === 'w') {
+      connection.write("Move: up");
+    }
+    if (key === 's') {
+      connection.write('Move: down');
+    }
+    if (key === 'a') {
+      connection.write('Move: left');
+    }
+    if (key === 'd') {
+      connection.write('Move: right');
+    }
+  })
 }
 
 module.exports = { setupInput };
