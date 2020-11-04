2de7744550b1dff14b1bf03d3699bef28526aea6
Jennifer Jung <jenjung2194@gmail.com>
2020-11-04 19:41:11 +0000

    added handleUserInput so we can terminate the game with Ctrl + C

--- play.js
+++ play.js
@@ -1,4 +1,27 @@
-const { connect } = require('./client')
+const { connect } = require('./client');
+const { stdin } = require('process');
 
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = () => {
+  stdin.on('data', key => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  })
+}
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  handleUserInput();
+
+  return stdin;
+}
+
+setupInput();
+
